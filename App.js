b7c85c5d42ffae444e25058badb8fff22952b12f
santiago ceballos <43916807+THEGLUM@users.noreply.github.com>
2021-05-21 18:50:38 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* dependecias */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React, { useState, useEffect } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { Text } from 'react-native'
+import InicioNavegation from './src/stack/InicioNavegation'
+
+//app
+
+const App = () => {
+	const [splash, setSplash] = useState(true)
+
+	//el tiempo del splash
+
+	return (
+		<NavigationContainer>
+			<InicioNavegation />
+		</NavigationContainer>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
